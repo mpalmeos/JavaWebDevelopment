4dce837c184529853b546bdfabfef24745e57edd
mpalmeos <maie.palmeos@itcollege.ee>
2018-10-11 06:54:43 +0300

    Modification on schema.sql - 3 out of 4 tests passed, fixed connection issue.

--- src/main/java/model/Rows.java
+++ src/main/java/model/Rows.java
@@ -7,4 +7,13 @@ public class Rows {
    private String itemName;
    private int quantity;
    private int price;
+
+   public Rows() {
+   }
+
+   public Rows(String itemName, int quantity, int price) {
+      this.itemName = itemName;
+      this.quantity = quantity;
+      this.price = price;
+   }
 }
