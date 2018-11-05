f1d6dfc88cb904f2a71683fe8116962a41bd1f64
mpalmeos <maie.palmeos@itcollege.ee>
2018-11-05 21:14:57 +0200

    OrderRows seem to be processed now.

--- src/main/java/dao/OrderDao.java
+++ src/main/java/dao/OrderDao.java
@@ -4,6 +4,7 @@ import model.Order;
 import model.Rows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Repository;
 
 import java.sql.*;
 import java.util.List;
+import java.util.Map;
 
 @Repository
 @Primary
@@ -19,26 +21,35 @@ public class OrderDao {
    @Autowired
    private JdbcTemplate template;
 
-   //https://codereview.stackexchange.com/questions/42185/how-to-fill-an-arraylist-of-arraylists-with-a-left-join
    public List<Order> getOrderList(){
-      String sql = "select o.id, o.orderNumber, r.itemName, r.quantity, r.price from orders o " +
-              "left join order_rows r on o.id = r.orders_id";
+      String sql = "select id, orderNumber from orders";
 
-      return template.query(sql, new BeanPropertyRowMapper<>(Order.class));
+      List<Order> orders = template.query(sql, new BeanPropertyRowMapper<>(Order.class));
+      for (Order o : orders) {
+         Long order_id = o.getId();
+         List<Rows> rowsList = getRows(order_id);
+         o.setOrderRows(rowsList);
+      }
+      return orders;
+   }
+
+   public List<Rows> getRows(Long orderID){
+      String sql = "select itemName, quantity, price from order_rows where orders_id = ?";
+      return template.query(sql, new Object[] {orderID}, new BeanPropertyRowMapper<>(Rows.class));
    }
 
    public Order getOrderByID(Long id){
 
-      String sql = "select o.id, o.orderNumber, r.itemName, r.quantity, r.price from orders o " +
-              "left join order_rows r on o.id = r.orders_id where o.id = ?";
+      String sql = "select id, orderNumber from orders where id = ?";
 
-      return template.queryForObject(sql, new Object[] {id}, new BeanPropertyRowMapper<>(Order.class));
+      Order order = template.queryForObject(sql, new Object[] {id}, new BeanPropertyRowMapper<>(Order.class));
+      List<Rows> rows = getRows(id);
+      order.setOrderRows(rows);
+      return order;
    }
 
    public Order insertOrder(Order order){
       String sql = "insert into orders (id, orderNumber) values (next value for seq1, ?)";
-      String sql_r = "insert into order_rows (row_id, itemName, quantity, price, orders_id) " +
-              "values (next value for seq2, ?, ?, ?, ?)";
 
       GeneratedKeyHolder id_holder = new GeneratedKeyHolder();
       Long order_id;
@@ -50,23 +61,32 @@ public class OrderDao {
       }, id_holder);
 
       order_id = id_holder.getKey().longValue();
-
-      if (order.getOrderRows() != null){
-         template.update(con -> {
-            PreparedStatement ps2 = con.prepareStatement(sql_r);
-            for (Rows r : order.getOrderRows()) {
-               ps2.setString(1, r.getItemName());
-               ps2.setInt(2, r.getQuantity());
-               ps2.setInt(3, r.getPrice());
-               ps2.setLong(4, order_id);
-               ps2.executeUpdate();
-            }
-            return ps2;
-         });
-      }
-
+      if (order.getOrderRows() != null) insertRows(order_id, order.getOrderRows());
       order.setId(order_id);
+
       return order;
+   }
+
+   //https://www.mkyong.com/spring/spring-jdbctemplate-batchupdate-example/
+   public void insertRows(Long orderID, List<Rows> rowsList){
+      String sql = "insert into order_rows (row_id, itemName, quantity, price, orders_id) " +
+              "values (next value for seq2, ?, ?, ?, ?)";
+
+      template.batchUpdate(sql, new BatchPreparedStatementSetter() {
+         @Override
+         public void setValues(PreparedStatement ps, int i) throws SQLException {
+            Rows r = rowsList.get(i);
+            ps.setString(1, r.getItemName());
+            ps.setInt(2, r.getQuantity());
+            ps.setInt(3, r.getPrice());
+            ps.setLong(4, orderID);
+         }
+
+         @Override
+         public int getBatchSize() {
+            return rowsList.size();
+         }
+      });
    }
 
    public void deleteOrderByID(Long id){
