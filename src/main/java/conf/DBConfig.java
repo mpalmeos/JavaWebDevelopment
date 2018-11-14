3c77c31bac7ae5fe36e964d7f9c7469e6124ccf7
mpalmeos <maie.palmeos@itcollege.ee>
2018-11-14 20:17:07 +0200

    Change to schema.sql and DBConfig in preparation for JPA.

--- src/main/java/conf/DBConfig.java
+++ src/main/java/conf/DBConfig.java
@@ -1,5 +1,6 @@
 package conf;
 
+import org.hibernate.jpa.HibernatePersistenceProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -8,11 +9,18 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import util.FileUtil;
 
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
+import java.util.Properties;
 
 @Configuration
+@EnableTransactionManagement
 @PropertySource("classpath:/application.properties")
 @ComponentScan(basePackages = {"dao"})
 public class DBConfig {
@@ -32,7 +40,32 @@ public class DBConfig {
    }
 
    @Bean
-   public JdbcTemplate getTemplate() {
-      return new JdbcTemplate(dataSource());
+   public PlatformTransactionManager transactionManager(
+           EntityManagerFactory entityManagerFactory) {
+
+      return new JpaTransactionManager(entityManagerFactory);
+   }
+
+   @Bean
+   public EntityManagerFactory entityManagerFactory() {
+      LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+      factory.setPersistenceProviderClass(HibernatePersistenceProvider.class);
+      factory.setPackagesToScan("model");
+      factory.setDataSource(dataSource());
+      factory.setJpaProperties(additionalProperties());
+      factory.afterPropertiesSet();
+
+      return factory.getObject();
+   }
+
+   private Properties additionalProperties() {
+      Properties properties = new Properties();
+      properties.setProperty("hibernate.hbm2ddl.auto", "validate");
+      properties.setProperty("hibernate.dialect",
+              "org.hibernate.dialect.HSQLDialect");
+      properties.setProperty("hibernate.show_sql", "true");
+      properties.setProperty("hibernate.format_sql", "true");
+
+      return properties;
    }
 }
